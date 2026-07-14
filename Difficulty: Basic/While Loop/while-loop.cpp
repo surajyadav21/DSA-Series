05bcd6ece93518565050a1ec6588895bb4bf1d9b
surajyadav21 <surajyadav884068@gmail.com>
2026-07-14 20:56:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void utility(int x) {
+        // code here
+        int i = 0;
+        while(x>=i){
+            cout<<x<<" ";
+            x = x-1;
+        }
+    }
+};
