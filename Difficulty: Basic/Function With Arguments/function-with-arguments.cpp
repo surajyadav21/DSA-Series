78e4651ba7211a6c2f5bec43092a003b79ff8049
surajyadav21 <surajyadav884068@gmail.com>
2026-07-15 17:25:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Arguments/function-with-arguments.cpp
@@ -0,0 +1,10 @@
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should return a+b
+class Solution {
+  public:
+    // code here
+    int argumentFunction(int a, int b){
+        return a+b;
+    }
+};
