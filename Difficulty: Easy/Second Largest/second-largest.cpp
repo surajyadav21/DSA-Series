a8873bf97eee7b8e11159add553e6dd17ec3dceb
surajyadav21 <surajyadav884068@gmail.com>
2026-07-21 12:18:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.cpp
+++ Difficulty: Easy/Second Largest/second-largest.cpp
@@ -4,13 +4,11 @@ class Solution {
         // code here
         int n = arr.size();
         sort(arr.begin(), arr.end());
-        for(int i=1; i<n; i++){
             for(int j=n-1; j>=1; j--){
                 if(arr[j-1] != arr[j]) {
                     return arr[j-1];
                 }
             }
-        }
         return -1;
     }
 };
