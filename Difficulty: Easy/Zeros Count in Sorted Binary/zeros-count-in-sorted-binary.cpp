0f8797ed09ec98500e8420ea3277e8c389c8afbf
surajyadav21 <surajyadav884068@gmail.com>
2026-08-20 11:19:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Zeros Count in Sorted Binary/zeros-count-in-sorted-binary.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int countZeroes(vector<int>& arr) {
+        int cnt=0;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]==0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
