7056fd557b4e42b569266afecca42320428b28cd
surajyadav21 <surajyadav884068@gmail.com>
2026-08-24 23:09:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.cpp
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -3,16 +3,13 @@ class Solution {
     int majorityElement(vector<int>& arr) {
         // code here
         sort(arr.begin(), arr.end());
-
         int n = arr.size();
         int candidate = arr[n / 2];
-
         int count = 0;
         for (int x : arr) {
             if (x == candidate)
                 count++;
             }
-
             if (count > n / 2)
                 return candidate;
         return -1;
