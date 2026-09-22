6e5b23ff556ff4f9140190932c477b9a44439107
surajyadav21 <surajyadav884068@gmail.com>
2026-09-22 14:27:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -1,7 +1,31 @@
 class Solution {
   public:
     void sort012(vector<int>& arr) {
-        // code here
-        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int cnt0=0, cnt1=0, cnt2=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                cnt0++;
+            }
+            else if(arr[i]==1){
+                cnt1++;
+            }
+            else{
+                cnt2++;
+            }
+        }
+        int idx = 0;
+        while(cnt0 > 0){
+            arr[idx] = 0;
+            idx++; cnt0--;
+        }
+        while(cnt1 > 0){
+            arr[idx] = 1;
+            idx++; cnt1--;
+        }
+        while(cnt2 > 0){
+            arr[idx] = 2;
+            idx++; cnt2--;
+        }
     }
 };
