39fc21f12e6b0face938c2b24882131c56eaaf73
surajyadav21 <surajyadav884068@gmail.com>
2026-09-23 09:59:18 +0530

    Time: 2 ms (51.13%), Space: 26.2 MB (47.35%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
+        vector<int> freq(n+1, 0);
         int dup = -1;
         int miss = -1;
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }
         for(int i=1; i<=n; i++){
-            int cnt=0;
-            for(int j=0; j<n; j++){
-                if(nums[j]==i){
-                    cnt++;
-                }
-            }
-            if(cnt == 0){
+            if(freq[i]==0){
                 miss = i;
             }
-            if(cnt == 2){
+            else if(freq[i]==2){
                 dup = i;
             }
         }
