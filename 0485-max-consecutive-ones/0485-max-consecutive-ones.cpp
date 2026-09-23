b2d92def69a117159491a03b1cc204fb8f4b1c4e
surajyadav21 <surajyadav884068@gmail.com>
2026-09-23 14:11:53 +0530

    Time: 3 ms (18.12%), Space: 50.2 MB (33.19%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,10 +6,11 @@ public:
         for(int i=0; i<nums.size(); i++){
             if(nums[i]==1){
                 cnt++;
-                maxi = max(cnt, maxi);
-            }else{
+            }
+            else{
                 cnt = 0;
             }
+            maxi = max(maxi, cnt);
         }
         return maxi;
     }
